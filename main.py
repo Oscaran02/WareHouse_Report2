a96bead2404312c70bc21da0417bbbfc2b9e531d
Oscar Pacheco <oscarpa_2000@hotmail.com>
2022-04-11 05:36:30 -0500

    new files upload

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from flask_cors import CORS
 
 app = Flask(__name__, static_url_path='',
             static_folder='static',
-            template_folder='template')
+            template_folder='templates')
 CORS(app)
 app.config["DEBUG"] = True
+app.config['host'] = '0.0.0.0'
+app.config['port'] = 5000
 
 
 @app.route("/home")
@@ -17,9 +19,16 @@ def home():
 def admin():
     return redirect(url_for("home"))
 
+
 @app.route("/")
 def default():
     return redirect(url_for("home"))
+
+
+@app.route("/upload")
+def upload():
+    return render_template("upload.html")
+
 
 if __name__ == '__main__':
     app.run()
