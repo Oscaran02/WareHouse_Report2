ed15e789c43f174bd3fa99fc0ed2d2e2a9490877
Oscar Pacheco <oscarpa_2000@hotmail.com>
2022-04-19 01:17:20 -0500

    some fixes
    Signed-off-by: Oscar Pacheco <oscarpa_2000@hotmail.com>

--- app.py
+++ app.py
@@ -37,75 +37,75 @@ def upload_file():
         f.save(filename)
         f.close()
         report_data.set_data()
-        sleep(5)
+        # sleep(5)
         return redirect(url_for('dashboard'))
 
 
 # Primera gráfica
 @app.route("/promedios_bodega", methods=['GET'])
 def promedios_bodega():
-    sleep(0.5)
+    # sleep(0.5)
     return report_data.average_times_in_warehouse().to_json(orient='records')
 
 
 # Segunda gráfica - Estado
 @app.route("/data2", methods=['GET'])
 def data2():
-    sleep(1)
+    # sleep(1)
     return report_data.state_of_package().to_json()
 
 
 # Tercera gráfica - Estado transito
 @app.route("/data3", methods=['GET'])
 def data3():
-    sleep(1.5)
+    # sleep(1.5)
     return report_data.state_of_package_in_transit().to_json()
 
 
 # Cuarta gráfica - Prealerta
 @app.route("/data4", methods=['GET'])
 def data4():
-    sleep(2.0)
+    # sleep(2.0)
     return report_data.prealerts().to_json()
 
 
 # Quinta gráfica - Origen
 @app.route("/data5", methods=['GET'])
 def data5():
-    sleep(2.5)
+    # sleep(2.5)
     return report_data.origin_of_package().to_json()
 
 
 # Sexta gráfica - Courier internacional
 @app.route("/data6", methods=['GET'])
 def data6():
-    sleep(3)
+    # sleep(3)
     return report_data.international_courier().to_json()
 
 
 # Séptima gráfica - Alianzas
 @app.route("/data7", methods=['GET'])
 def data7():
-    sleep(3.5)
+    # sleep(3.5)
     return report_data.alliance_of_package().to_json()
 
 
 # Octava gráfica - Courier local
 @app.route("/data8", methods=['GET'])
 def data8():
-    sleep(4)
+    # sleep(4)
     return report_data.local_courier().to_json()
 
 
 # Novena gráfica - Departamentos
 @app.route("/data9", methods=['GET'])
 def data9():
-    sleep(4.5)
+    # sleep(4.5)
     return report_data.department_of_customer().to_json()
 
 
 # Décima gráfica - Tiempos totales
 @app.route("/data10", methods=['GET'])
 def data10():
-    sleep(5)
+    # sleep(5)
     return report_data.average_time_in_routes().to_json()
